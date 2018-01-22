1aab804a84f1ac6bb7b8adeae61a9a41d4505fb0
rishi dutta <rda@kanakinfosystems.com>
2018-01-22 20:46:41 +0530

    [update]

--- music/views.py
+++ music/views.py
@@ -23,6 +23,18 @@ def detail(request, album_id):
         album = Album.objects.get(pk=album_id)
     except Album.DoesNotExist:
         raise Http404("Album does not exist")
-    return render(request, 'music/detail.html', {'album': album}
+    return render(request, 'music/detail.html', {'album': album})
 
-
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.Post['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html',{
+            'album': album,
+            'error_message': "You did not select a valid song",
+            })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request,'music/detail.html', {'album': album})
